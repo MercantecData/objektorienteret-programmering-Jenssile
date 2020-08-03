6e271bfb0533f5f8849dfc9e9f6fab6619baf8d4
Jens <jens.e.bouwens@gmail.com>
2020-08-03 13:38:47 +0200

    files added

---
+++ Projects H2/Person/Person.cs
@@ -0,0 +1,23 @@
+﻿using Jobs;
+using System.Runtime.InteropServices;
+
+namespace People
+{
+    public class Person // is used in the personregisterTest
+    {
+        public string name;
+        public int age;
+        public int personNr;
+        public string adress;
+        public Job job;
+
+        public Person(string name, int age, int personNr, string adress, string position, double salary, string employer, string workAdress)
+        {
+            this.name = name;
+            this.age = age;
+            this.personNr = personNr;
+            this.adress = adress;
+            this.job = new Job(position, salary, employer, workAdress);
+        }
+    }
+}
---
+++ Projects H2/PersonRegisterTest/Program.cs
@@ -0,0 +1,66 @@
+﻿using People;
+using System;
+using System.Collections.Generic;
+
+namespace PersonRegisterTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Person> peoples = new List<Person>
+            {
+                new Person("bob", 20, 293749, "somewhere", "janitor", 700, "merc", "here"),
+                new Person("tess", 200, 1237, "somewhere", "janitor", 705670, "merc", "here"),
+                new Person("joke", 19, 91237, "somewhere", "janitor", 70230, "merc", "here")
+            };
+
+            foreach (var item in peoples)
+            {
+                Console.WriteLine("name: " + item.name);
+                Console.WriteLine("age: " + item.age);
+                Console.WriteLine("salary: " + item.job.salary);
+                Console.WriteLine("---------------------");
+            }
+
+            Console.WriteLine("add new person.");
+            Console.WriteLine("write name.");
+            string name = Console.ReadLine().ToString();
+
+            Console.WriteLine("---");
+            Console.WriteLine("write age.");
+            int age = Convert.ToInt32(Console.ReadLine().ToString().Trim());
+
+            Console.WriteLine("---");
+            Console.WriteLine("write person number.");
+            int personNr = Convert.ToInt32(Console.ReadLine().ToString().Trim());
+
+            Console.WriteLine("---");
+            Console.WriteLine("write adress.");
+            string adress = Console.ReadLine().ToString();
+
+            Console.WriteLine("---");
+            Console.WriteLine("write job.");
+            string job = Console.ReadLine().ToString();
+
+            Console.WriteLine("---");
+            Console.WriteLine("write salary.");
+            double salary = Convert.ToDouble(Console.ReadLine().Trim());
+
+            Console.WriteLine("---");
+            Console.WriteLine("write employer.");
+            string employer = Console.ReadLine().ToString();
+
+            Console.WriteLine("---");
+            Console.WriteLine("write work adress.");
+            string workAdress = Console.ReadLine().ToString();
+
+            AddPersonToList(name, age, personNr, adress, job, salary, employer, workAdress, peoples);
+        }
+
+        public void AddPersonToList(string name, int age, int personNr, string adress, string job, double salary, string employer, string workAdress, List<Person> peoples)
+        {
+            peoples.Add(new Person(name, age, personNr, adress, job, salary, employer, workAdress));
+        }
+    }
+}

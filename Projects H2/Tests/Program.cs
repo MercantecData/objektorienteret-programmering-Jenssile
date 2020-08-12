1d12e0ced3e18b6fde2b3b0ba30c9cca58ddc585
Jens <jens.e.bouwens@gmail.com>
2020-08-12 12:30:42 +0200

    some more progress

---
+++ Projects H2/Tests/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    class Program : Timer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You have 10 seconds to guess a number between 1 and 10");
+            Console.WriteLine("press enter to begin.");
+            Console.ReadLine();
+
+            int j = 3;
+            var countdown = Countdown(j);
+            Task.WaitAll(countdown);
+
+            j = 10;
+            Reminder(j, () => Console.Write("seconds left: "));
+            Console.ReadLine();
+        }
+    }
+}
